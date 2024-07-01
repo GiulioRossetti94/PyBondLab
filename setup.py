573ea1ffbc0b3bde726672539b4537540c491dd2
GiulioRossetti94 <giulio.rossetti94@gmail.com>
2024-07-01 15:26:25 +0100

    added long desc

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 setup(name = 'PyBondLab',
       version = '0.0.1',
       description = ('Performs portfolio sorting and strategy evaluation for corporate bonds'),
+      long_description=open('README.md').read(),
       author = 'Giulio Rossetti, Alex Dickerson',
       author_email = 'Giulio.Rossetti.1@wbs.ac.uk, alexander.dickerson1@unsw.edu.au',
       license='MIT',
       keywords='corporate bonds, portfolio sorting, data cleaning',
       packages=['PyBondLab'],
+    #   packages=find_packages(),
       url='https://github.com/GiulioRossetti94/PyBondLab',
       install_requires=[
           'numpy','pandas'],
