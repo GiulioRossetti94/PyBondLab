f7747f816c9e8483ce7c568b1046d7e977cd6f02
Giulio Rossetti <giulio14__@hotmail.it>
2024-07-12 12:33:06 +0200

    breakpoints

--- PyBondLab/data/data_loading.py
+++ PyBondLab/data/data_loading.py
@@ -18,11 +18,12 @@ def load_data(file_root:str,file_name: str) -> pd.DataFrame:
         The data loaded from the file
     """
     file_path = os.path.split(os.path.abspath(file_root))[0]
-    data = pd.read_csv(os.path.join(file_path,file_name))
-    if "date" in data.columns:
-        data["date"] = pd.to_datetime(data["date"], dayfirst=True)
-        data = data.set_index("date")
+    data = pd.read_csv(os.path.join(file_path,file_name),index_col=0)
+    data.index = pd.to_datetime(data.index)
+    # if "date" in data.columns:
+    #     data["date"] = pd.to_datetime(data["date"], dayfirst=True)
+    #     data = data.set_index("date")
 
-    for col in data:
-        data[col] = pd.to_numeric(data[col], errors='coerce')
+    # for col in data:
+    #     data[col] = pd.to_numeric(data[col], errors='coerce')
     return data
